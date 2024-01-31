649098357045d504cc0f639f917b90ea438b5c2e
1001hadi <hadi.jaf.1001@gmail.com>
2024-01-31 14:26:14 -0700

    books table added to UI

--- api/index.js
+++ api/index.js
@@ -9,14 +9,14 @@ const app = express();
 // Middleware
 app.use(express.json());
 // handdle cors middleware
-// app.use(cors())
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type"],
-  })
-);
+app.use(cors());
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type"],
+//   })
+// );
 
 app.get("/", (req, res) => {
   console.log(req);
