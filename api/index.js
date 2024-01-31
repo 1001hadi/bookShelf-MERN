0117ccc645ca4405093914d0baf9d7d3d0b2273d
1001hadi <hadi.jaf.1001@gmail.com>
2024-01-31 12:38:16 -0700

    CRUD operation has been setup

--- api/index.js
+++ api/index.js
@@ -48,6 +48,55 @@ app.get("/books", async (req, res) => {
   }
 });
 
+// getSingle by id
+app.get("/books/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const book = await Book.findById(id);
+    return res.status(200).json(book);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send({ message: err.message });
+  }
+});
+
+// update
+app.put("/books/:id", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      return res
+        .status(400)
+        .send({ message: "Required fields: title, author, publishYear" });
+    }
+    const { id } = req.params;
+    const result = await Book.findByIdAndUpdate(id, req.body);
+
+    if (!result) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+    return res.status(200).send({ message: "Book Successfully Updated" });
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send({ message: err.message });
+  }
+});
+
+// delete with id
+app.delete("/books/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const removedBook = await Book.findByIdAndDelete(id);
+
+    if (!removedBook) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+    return res.status(200).send({ message: "Book Successfully Deleted" });
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send({ message: err.message });
+  }
+});
+
 mongoose
   .connect(mongoDBURL)
   .then(() => {
